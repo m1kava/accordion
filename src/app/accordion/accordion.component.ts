28b984f73b4afc4dabb94b9d361c52f0e93038cc
Mikava <mikava.vova@gmail.com>
2021-01-22 23:19:34 +0400

    JSON mapping

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,24 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+interface AccordionResponseModel {
+  code: number;
+  data: Array<AccordionModel>;
+  description: string;
+}
+interface AccordionModel {
+  categoryFlag: string;
+  categoryId: number;
+  categoryName: string;
+  eventsCount: number;
+  level: number;
+  parentCategory: number;
+  parentName: string;
+  remoteId: number;
+  sortOrder: number;
+  sportId: number;
+  sportName: string;
+  treatAsSport: number;
+}
+
+interface AccordionDataModel {
+  name: string;
+  eventsCount: number;
+  sortOrder: number;
+  children: Array<{
+    name: string;
+    eventsCount: number;
+    sortOrder: number;
+    children: Array<{
+      name: string;
+      sortOrder: number;
+    }>
+  }>
+}
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.scss']
 })
-export class AccordionComponent {
-  url ='https://www.lionsbet.com/rest/market/categories';
-  items = [];
+export class AccordionComponent implements OnInit {
+  private url: string ='https://www.lionsbet.com/rest/market/categories';
 
-  constructor(private http: HttpClient) { 
-    this.http.get(this.url).toPromise().then(data => {
-      console.log(data);
+  public accordionData: AccordionDataModel[] = [];
 
-      for (let key in data) {
-        if (data.hasOwnProperty(key)) {
-          this.items.push(data[key]);
-        }
-      }
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get(this.url).subscribe((res: AccordionResponseModel) => {
+      this.accordionData = this.sortData(res.data);
     });
+  }
+
+  sortData(data: AccordionModel[]): AccordionDataModel[] {
+    return data
+      .filter((category: AccordionModel) => category.level === 1)
+      .map((category: AccordionModel) => {
+        return {
+          name: category.categoryName,
+          eventsCount: category.eventsCount,
+          sortOrder: category.sortOrder,
+          children: data
+            .filter((country: AccordionModel) => country.level === 2 && country.parentCategory === category.categoryId)
+            .map((country: AccordionModel) => {
+              return {
+                name: country.categoryName,
+                eventsCount: country.eventsCount,
+                sortOrder: country.sortOrder,
+                children: data
+                  .filter((championship: AccordionModel) => championship.level === 3 && championship.parentCategory === country.categoryId)
+                  .map((championship: AccordionModel) => {
+                    return {
+                      name: championship.categoryName,
+                      sortOrder: championship.sortOrder
+                    };
+                  })
+                  .sort((a, b) => a.sortOrder - b.sortOrder)
+              };
+            })
+            .sort((a, b) => a.sortOrder - b.sortOrder)
+        };
+      })
+      .sort((a, b) => a.sortOrder - b.sortOrder);
   }
 }
